c32f7c9b670978d8dc7d90610168495795abeb92
Brian Richter <pokebrian@gmail.com>
2012-03-06 22:54:42 -0300

    adicionado diagrama e iniciado bundle internote

---
+++ src/Internote/NoteBundle/Entity/Note.php
@@ -0,0 +1,78 @@
+<?php
+namespace Internote\NoteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="note")
+ */
+class Note
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+
+	/**
+	 * @ORM\Column(type="string", length=100)
+	 */
+	protected $name;
+
+	/**
+	 * @ORM\Column(type="text")
+	 */
+	protected $text;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set text
+     *
+     * @param text $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * Get text
+     *
+     * @return text 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
---
+++ src/Internote/NoteBundle/InternoteNoteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Internote\NoteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class InternoteNoteBundle extends Bundle
+{
+}
