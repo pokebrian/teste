a29368561571b191a71c0cf4f079a552fc38060d
Brian Richter <pokebrian@gmail.com>
2012-03-12 22:25:46 -0300

    alterado services.xml para services yml; listando notes;

--- src/Internote/NoteBundle/Controller/NoteListController.php
+++ src/Internote/NoteBundle/Controller/NoteListController.php
@@ -4,13 +4,23 @@ namespace Internote\NoteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\Response;
 
-class MainController extends Controller
+class NoteListController extends Controller
 {
 	public function listAction()
 	{
 		//return new Response('<html><body>Esta é a página inicial, <strong>Bem Vindo!</strong></body></html>');
 
-		return $this->render('NerdHerdHelloBundle:Note:notelist.html.twig', array('name' => "teste"));
+		
+// 		$entityManager = $this->get('doctrine.orm.entity_manager');
+// 		$product = $entityManager->find('NerdHerd\HelloBundle\Entity\Product', 2);
+		
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		$query = $em->createQuery('SELECT u FROM Internote\NoteBundle\Entity\Note u');// WHERE u.age > 20');
+		$notes = $query->getResult();
+		
+		
+		return $this->render('InternoteNoteBundle:Note:notelist.html.twig', array('notes' => $notes));
 		//		                      AcmeHelloBundle:Hello:index.html.twig
 	}
 
